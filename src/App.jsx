b29b9de26e1f9c6beaf56b527b72e2ed1fff5a77
Débora <dboravitoriasilva16@gmail.com>
2025-07-07 11:05:03 -0300

    Iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+      <div className="bg-[#242424]">
+          <h1 className="text-red-800">Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+
